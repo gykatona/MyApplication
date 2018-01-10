4bb2ed21c80ca555255c21a61aba99f931e4bb6f
KatonaGyorgy <gy.katona7@gmail.com>
2018-01-10 10:28:51 +0100

    second commit

--- app/src/main/java/com/example/katonagyorgy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/katonagyorgy/myapplication/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.katonagyorgy.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView tv1 = (TextView) findViewById(R.id.textView2);
+        Button btn1 = (Button) findViewById(R.id.button);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DisplayMessageActivity.class);
+
+                String message = tv1.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            }
+        });
     }
 }
